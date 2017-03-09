61f4ef9672659abd8a5177d230f841a8d39b5612
PirunSeng <sengpirun.rain@gmail.com>
2017-03-09 10:09:33 +0700

    Update regex of last string

--- app/models/finder.rb
+++ app/models/finder.rb
@@ -4,7 +4,7 @@ class Finder < ActiveRecord::Base
 
   validates :name, :reference, presence: true, uniqueness: { case_sensitive: false }
   validates_format_of :reference, with: /\A(ftp|http|https)\:\/\/[a-z]+(\W)*([a-z])*[.]([a-z]+)(.*)[a-z]\z/, message: 'should match "https://www.myexample.com"'
-
+  # /\A(ftp|http|https)\:\/\/[a-z]+(\W)*([a-z])*[.]([a-z]+)(.*)\w\z/, is better than above.
   default_scope { order(created_at: :desc) }
 
   scope :name_like,        -> (value) { where('LOWER(finders.name) LIKE ?', "%#{value.downcase}%") }
